bc9f373cc53efc5ed9c4d6abf284b211e55cf756
seth <seth.patterson01@gmail.com>
2025-08-08 14:22:38 -0600

    Update2.22

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ function toggleMenu() {
   const navLinks = document.getElementById('navLinks');
   navLinks.classList.toggle('active');
 }
+if (window.location.pathname === '/index.html') {
+    window.history.replaceState({}, document.title, '/');
+}
