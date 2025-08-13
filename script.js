6b90830decb5b9ac1793a398d751db0720b13b04
seth <seth.patterson01@gmail.com>
2025-08-13 00:54:28 -0600

    FinalWebsite v1

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const email = 'test@yahoo.com';
+
 function toggleMenu() {
   const navLinks = document.getElementById('navLinks');
   navLinks.classList.toggle('active');
@@ -5,3 +7,11 @@ function toggleMenu() {
 if (window.location.pathname === '/index.html') {
     window.history.replaceState({}, document.title, '/');
 }
+function sendEmail() {
+    const itemTitle = document.querySelector(".item-title").textContent.trim();
+    const subject = encodeURIComponent(`Inquiry about ${itemTitle}`);
+    const body = encodeURIComponent(
+      `Hi Draft Horse Art,\n\nI'm interested in purchasing the ${itemTitle} art. Please let me know how to proceed.\n\nThank you!`
+    );
+    window.location.href = `mailto:${email}?subject=${subject}&body=${body}`;
+  }
